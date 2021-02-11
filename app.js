124e58fddc3f004be49b7b683a9c0fb1d3a257f3
Rachel-Reidenga <reidenga@gmail.com>
2021-02-10 20:47:30 -0600

    promt questions

--- app.js
+++ app.js
@@ -1,14 +1,46 @@
 const inquirer = require("inquirer");
 
-inquirer
-  .prompt([
+const promtProject = portfolioData => {
+  console.log('');
+  return inquirer.prompt([
     {
       type: "input",
       name: "name",
       message: "What is your name?"
-    }
-  ])
-  .then( answers => console.log(answers));
+    },
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the name of your project?'
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of the project (Required):'
+    },
+    {
+      type: 'checkbox',
+      name: 'languages',
+      message: 'What did you build this project with? (Check all that apply)',
+      choices: ["JavaScript", "HTML", "CSS", "ES6", "jQuery", "Bootstrap", "Node"]
+    },
+    {
+      type: 'input',
+      name: 'link',
+      message: 'Would you like to feature this project?',
+      default: false
+    },
+  ]);
+
+
+
+};
+  promtUser()
+    .then( answers => console.log(answers))
+    .then(promtProject)
+    .then(projectAnswers => console.log(projectAnswers));
+
+  
 
 // const fs = require('fs');
 // const generatePage = require('./src/page-template');
